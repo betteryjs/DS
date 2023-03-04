cfac89e17d02113e8a373444a3584dae6fd0287b
betteryjs <aanbnbnbnb@gmail.com>
2023-03-04 10:24:09 +0800

    fix all

--- NK/第4章 链表/src/cpp/reverseList.cpp
+++ NK/第4章 链表/src/cpp/reverseList.cpp
@@ -19,32 +19,55 @@ struct ListNode {
 };
 
 
+struct DoubleNode {
+    int value;
+    DoubleNode *last;
+    DoubleNode *next;
 
-ListNode*   reverseList(ListNode * head){
-    ListNode * pre= nullptr;
-    ListNode * cur=head;
-    while (cur!= nullptr){
-        ListNode * nextCur=cur->next;
-        cur->next=pre;
-        pre=cur;
-        cur=nextCur;
+    DoubleNode(int data) : value(data) {}
+};
+
+ListNode *reverseList(ListNode *head) {
+    ListNode *pre = nullptr;
+    ListNode *cur = head;
+    while (cur != nullptr) {
+        ListNode *nextCur = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = nextCur;
     }
     return pre;
 }
 
 
-ListNode*   reverseListAtoB(ListNode * head, ListNode * end){
-    ListNode * pre= nullptr;
-    ListNode * cur=head;
-    while (cur!= end){
-        ListNode * nextCur=cur->next;
-        cur->next=pre;
-        pre=cur;
-        cur=nextCur;
+ListNode *reverseListAtoB(ListNode *head, ListNode *end) {
+    ListNode *pre = nullptr;
+    ListNode *cur = head;
+    while (cur != end) {
+        ListNode *nextCur = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = nextCur;
     }
     return pre;
 }
 
+
+
+
+DoubleNode * reverseDoubleList(DoubleNode*  head) {
+    DoubleNode*  pre = nullptr;
+    DoubleNode* nextCur = nullptr;
+    DoubleNode * cur=head;
+    while (cur != nullptr) {
+        nextCur = cur->next;
+        cur->next = pre;
+        cur->last = nextCur;
+        pre = cur;
+        cur = nextCur;
+    }
+    return pre;
+}
 
 
 
