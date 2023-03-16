1c94873235b0147dd1da309f0196e32a15c65901
betteryjs <aanbnbnbnb@gmail.com>
2023-03-16 08:16:43 +0800

    *.cpp

--- NK/链表/src/cpp/CopyListWithRandom.cpp
+++ NK/链表/src/cpp/CopyListWithRandom.cpp
@@ -2,6 +2,7 @@
 // Created by yjs on 23-3-15.
 // 复制含有随机指针节点的链表
 //
+
 #include <iostream>
 #include <unordered_map>
 
@@ -40,6 +41,41 @@ ListNode *copyListWithRandMap(ListNode *head) {
 
 
 ListNode *copyListWithRand(ListNode *head) {
+    if(head== nullptr){
+        return nullptr;
+    }
+    ListNode * cur=head;
+    ListNode * nextNode= nullptr;
+
+    while (cur!= nullptr){
+        nextNode=cur->next;
+        cur->next=new ListNode(cur->val);
+        cur->next->next=nextNode;
+        cur=nextNode;
+    }
+    // 1  - 1'  - 2  - 2' - 3  - 3'
+    cur=head;
+    ListNode  * copyHead= head->next;
+
+    // set copy node rand
+    // 1 -  1' -  2 - 2' - 3  - 3'
+    while (cur!= nullptr){
+        nextNode=cur->next->next;
+        copyHead=cur->next;
+        cur->next=nextNode;
+        copyHead->next= nextNode== nullptr ? nullptr:nextNode->next;
+        cur=nextNode;
+    }
+
+    return copyHead;
+
+
+
+
+
+
+
+
 
 
 
