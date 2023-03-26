a1eebb371e22e325d4475c74c7198c09b737efba
betteryjs <aanbnbnbnb@gmail.com>
2023-03-26 23:41:09 +0800

    *.cpp

--- NK/链表/src/cpp/相交有环节点.cpp
+++ NK/链表/src/cpp/相交有环节点.cpp
@@ -87,6 +87,55 @@ ListNode * getCommonNode(ListNode * head1,ListNode * head2){
 }
 
 
+ListNode * bothLoop(ListNode * head1,ListNode * loop1 , ListNode * head2,ListNode * loop2){
+    ListNode * cur1,*cur2;
+    if(loop1==loop2){
+        // 第二种情况 无环链表相交问题
+        cur1 = head1;
+        cur2 = head2;
+        int n = 0;
+        while (cur1 != loop1) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2 != loop2) {
+            n--;
+            cur2 = cur2.next;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n =abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }else{
+        cur1=loop1->next;
+        while (cur1!=loop1){
+            if(cur1==loop2){
+                // 情况3
+                return loop1;
+            }
+
+
+            cur1=cur1->next;
+        }
+        // 情况1 相交节点为空
+        return nullptr;
+
+
+
+    }
+
+
+
+}
+
 ListNode * getIntersectNode(ListNode * head1,ListNode * head2){
 
     if (head1 == nullptr || head2 == nullptr) {
@@ -99,9 +148,7 @@ ListNode * getIntersectNode(ListNode * head1,ListNode * head2){
         return getCommonNode(head1, head2);
     }
     if(loop1!= nullptr && loop2!= nullptr){
-
-
-
+        return bothLoop(head1,loop1,head2,loop2);
     }
     // 一个链表有环另外一个无环 一定不相交
 
